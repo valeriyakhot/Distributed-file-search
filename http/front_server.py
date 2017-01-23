5ba122ced0a3076113d419d5118a30acc2f05a16
valeria <valeria.ho66@leobaeck.net>
2017-01-23 14:13:20 +0200

    project 23.1

---
+++ http/front_server.py
@@ -0,0 +1,292 @@
+# -*- coding: utf-8 -*-
+
+import tempfile
+import argparse
+import contextlib
+import errno
+import os
+import socket
+import sys
+import traceback
+import urlparse
+import xml.etree.cElementTree as ET
+
+#C:\cygwin64\tmp>python -m http.front_server --url http://localhost:8070/\ --bind-port 8080 --node-port 8070
+
+from .common import constants
+from .common import util
+from .common import send_it
+
+HTML_TABLE_HEADER='''<!DOCTYPE html><html><body><table style="width:35%" ; border= "2px solid #dddddd">  <tr>    <th align="left"> Filename </th>    <th align="left">Option</th>   </tr>'''
+HTML_END='''</table></body></html>'''
+HTML_SEARCH='search_form.html'
+URI_SEARCH='/search?Search='
+URI_ID='/get_file?id='
+  
+
+def parse_args():
+    """Parse program argument."""
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--base',
+        default='.',
+        help='Base directory to search fils in, default: %(default)s',
+    )
+    parser.add_argument(
+        '--bind-address',
+        default='0.0.0.0',
+        help='Bind address, default: %(default)s',
+    )
+    parser.add_argument(
+        '--bind-port',
+        default=0,
+        type=int,
+        help='Bind port, default: %(default)s',
+    )
+    parser.add_argument(
+        '--node-port',
+        default=0,
+        type=int,
+        help='Bind port, default: %(default)s',
+    )
+    parser.add_argument(
+        '--url',
+        required=True,
+        help='URL to use',
+    )
+    return parser.parse_args()
+
+def xml_to_html(st):
+    st=str(st)
+    root=ET.fromstring(st)
+    html=HTML_TABLE_HEADER
+    for result in root.findall('result'):
+        name = result.get('name')
+        id=result.get('id')
+        html+='  <tr> <td>%s</td> <td align="middle"> <a href="/download_file?id=%s">&lt;download&gt;</a> <a href="/view_file?id=%s">&lt;view&gt;</a></td>  </tr>' %(name,id,id)
+    html+='<a href="/form?">&lt;Back&gt;</a>'+HTML_END
+    return html
+    
+
+def server():  
+    args = parse_args()
+    print('start')
+    with contextlib.closing(
+        socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+        )
+    ) as sl:
+        sl.bind((args.bind_address, args.bind_port))
+        sl.listen(10)
+        print 'connect'
+        while True:
+            s, addr = sl.accept()
+            with contextlib.closing(s):
+                status_sent = True
+                try:
+                    rest = bytearray()
+                       
+                    req, rest = util.recv_line(s, rest)
+                    req_comps = req.split(' ', 2)
+                    if req_comps[2] != constants.HTTP_SIGNATURE:
+                        raise RuntimeError('Not HTTP protocol')
+                    if len(req_comps) != 3:
+                        raise RuntimeError('Incomplete HTTP protocol')
+
+                    method, uri, signature = req_comps
+                    if method != 'GET':
+                        raise RuntimeError(
+                            "HTTP unsupported method '%s'" % method
+                        )
+
+
+                    #
+                    # Create a file out of request uri.
+                    # Be extra careful, it must not escape
+                    # the base path.
+                    #
+                    # NOTICE: os.path.normpath cannot be used checkout:
+                    # os.path.normpath(('/a/b', '/a/b1')
+                    #
+                    if not uri or uri[0] != '/':
+                        raise RuntimeError("Invalid URI")
+                    file_name = os.path.normpath(
+                        os.path.join(
+                            args.base,
+                            uri[1:],
+                        )
+                    )
+                    
+                    print(uri)
+                    
+                    parse = urlparse.urlparse(uri)
+                    param_temp = parse.query
+                    param = urlparse.parse_qs(urlparse.urlparse(uri).query).values()
+                    
+                    if uri[:15]==URI_SEARCH:
+                        print ('I do search')
+                        if len(uri) != len(URI_SEARCH):
+                            output=client(URI_SEARCH,param[0][0],True) 
+                    elif uri[:11]=='/view_file?':
+                        output=client(URI_ID,param[0][0],False)
+                    elif uri[:15]=='/download_file?':
+                        output=client(URI_ID,param[0][0],False)
+                        send_it.download(s,output)
+                        
+
+                    elif uri[:6]=='/form?':
+                    
+                        send_it.send_file(s,HTML_SEARCH)       
+                        
+                    else :
+                        output =''
+
+
+                    send_it.send(s,output)
+
+
+                except IOError as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        if e.errno == errno.ENOENT:
+                            send_it.send_status(s, 404, 'File Not Found', e)
+                        else:
+                            send_it.send_status(s, 500, 'Internal Error', e)
+                except Exception as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        send_it.send_status(s, 500, 'Internal Error', e)
+
+
+                        
+def client(uri_beg,search,xml_status):
+    args = parse_args()
+    url = util.spliturl(args.url)
+    if url.scheme != 'http':
+        raise RuntimeError("Invalid URL scheme '%s'" % url.scheme)
+
+    with contextlib.closing(
+        socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+        )
+    ) as s:
+        print  (str(url.hostname)+ str(url.port if url.port else constants.DEFAULT_HTTP_PORT))
+        s.connect((
+            url.hostname,
+            url.port if url.port else constants.DEFAULT_HTTP_PORT,
+        ))
+        uri=uri_beg+search
+        util.send_all(
+            s,
+            (
+                (
+                    'GET %s HTTP/1.1\r\n'
+                    'Host: %s\r\n'
+                    '\r\n'
+                ) % (
+                    uri,
+                    args.url+uri,
+                )
+            ).encode('utf-8'),
+        )
+
+        rest = bytearray()
+
+        #
+        # Parse status line
+        #
+        status, rest = util.recv_line(s, rest)
+        status_comps = status.split(' ', 2)
+        if status_comps[0] != constants.HTTP_SIGNATURE:
+            raise RuntimeError('Not HTTP protocol')
+        if len(status_comps) != 3:
+            raise RuntimeError('Incomplete HTTP protocol')
+
+        signature, code, message = status_comps
+        if code != '200':
+            raise RuntimeError('HTTP failure %s: %s' % (code, message))
+
+        #
+        # Parse headers
+        #
+        content_length = None
+        for i in range(constants.MAX_NUMBER_OF_HEADERS):
+            line, rest = util.recv_line(s, rest)
+            if not line:
+                break
+
+            name, value = util.parse_header(line)
+            if name == 'Content-Length':
+                content_length = int(value)
+        else:
+            raise RuntimeError('Too many headers')
+
+        #
+        # Write content
+        #
+        # Do not write output file directly
+        # Create temporary file at same directory
+        # Only if all OK we overwrite the file
+        #
+        
+        try:
+            if content_length is None:
+                # Fast track, no content length
+                # Recv until disconnect
+                buf=''
+                while True:
+                    buf += s.recv(constants.BLOCK_SIZE)
+                    if not buf:
+                        break
+                if xml_status:
+                    output=xml_to_html(buf)
+                else:
+                    output=buf
+                return output
+            else:
+                buff=''
+                
+                # Safe track, we have content length
+                # Recv excacly what requested.
+                print content_length
+                left_to_read = content_length
+                while left_to_read > 0:
+                    if not rest:
+                        t = s.recv(constants.BLOCK_SIZE)
+                        if not t:
+                            raise RuntimeError(
+                                'Disconnected while waiting for content'
+                            )
+                        rest += t
+                    buf, rest = rest[:left_to_read], rest[left_to_read:]
+                    buff+=buf
+                    left_to_read -= len(buf)
+                    print len(buff)
+                if xml_status:
+                    output=xml_to_html(buff)
+                else:
+                    output=buff
+                
+                return output
+
+            # Commit
+            name = None
+        finally:
+            if name is not None:
+                try:
+                    os.remove(name)
+                except Exception:
+                    print("Cannot remove temp file '%s'" % name)
+
+    
+def main():
+    server()
+
+if __name__ == '__main__':
+    main()
+
+
+# vim: expandtab tabstop=4 shiftwidth=4
---
+++ http/node_server.py
@@ -0,0 +1,176 @@
+#/usr/bin/python
+
+
+import argparse
+import contextlib
+import errno
+import os
+import socket
+import sys
+import traceback
+import urlparse
+import xml.etree.cElementTree as ET
+
+#C:\cygwin64\tmp>python -m http.node_server --bind-port 8070
+
+from .common import constants
+from .common import util
+from .common import send_it
+
+
+DIRECTORY='./'
+
+
+def parse_args():
+    """Parse program argument."""
+
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--bind-address',
+        default='0.0.0.0',
+        help='Bind address, default: %(default)s',
+    )
+    parser.add_argument(
+        '--bind-port',
+        default=constants.DEFAULT_HTTP_PORT,
+        type=int,
+        help='Bind port, default: %(default)s',
+    )
+    parser.add_argument(
+        '--base',
+        default='.',
+        help='Base directory to search fils in, default: %(default)s',
+    )
+    args = parser.parse_args()
+    args.base = os.path.normpath(os.path.realpath(args.base))
+    return args
+    
+def server():  
+    args = parse_args()
+    print('start')
+    with contextlib.closing(
+        socket.socket(
+            family=socket.AF_INET,
+            type=socket.SOCK_STREAM,
+        )
+    ) as sl:
+        sl.bind((args.bind_address, args.bind_port))
+        sl.listen(10)
+        while True:
+            s, addr = sl.accept()
+            with contextlib.closing(s):
+                status_sent = True
+                try:
+                    rest = bytearray()
+
+
+                    #
+                    # Parse request line
+                    #
+                    
+                    req, rest = util.recv_line(s, rest)
+                    req_comps = req.split(' ', 2)
+                    if req_comps[2] != constants.HTTP_SIGNATURE:
+                        raise RuntimeError('Not HTTP protocol')
+                    if len(req_comps) != 3:
+                        raise RuntimeError('Incomplete HTTP protocol')
+
+                    method, uri, signature = req_comps
+                    if method != 'GET':
+                        raise RuntimeError(
+                            "HTTP unsupported method '%s'" % method
+                        )
+
+
+                    #
+                    # Create a file out of request uri.
+                    # Be extra careful, it must not escape
+                    # the base path.
+                    #
+                    # NOTICE: os.path.normpath cannot be used checkout:
+                    # os.path.normpath(('/a/b', '/a/b1')
+                    #
+                    if not uri or uri[0] != '/':
+                        raise RuntimeError("Invalid URI")
+                    file_name = os.path.normpath(
+                        os.path.join(
+                            args.base,
+                            uri[1:],
+                        )
+                    )
+                    
+                    
+                    parse = urlparse.urlparse(uri)
+                    param_temp = parse.query
+                    param = urlparse.parse_qs(urlparse.urlparse(uri).query).values()
+                    mem=mem_list()
+                    normal_out=True
+                    if uri[:8]=='/search?':
+                        files,ids= find_name(mem,param[0][0])
+                        output=xml_form(files,ids)
+                    elif uri[:15]=='/direct_search?':
+                        files,ids= find_name(mem,param[0][0],True)
+                        output=xml_form(files,ids)
+                    elif uri[:10]=='/get_file?':
+                        normal_out=False
+                        file_name = os.path.join(mem[int(param[0][0])]['root'],mem[int(param[0][0])]['filename'])
+                        send_it.send_file(s,file_name)
+                    else:
+                        output=''
+
+                    if normal_out:
+                        send_it.send_xml(s,output)
+
+                except IOError as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        if e.errno == errno.ENOENT:
+                            send_it.send_status(s, 404, 'File Not Found', e)
+                        else:
+                            send_it.send_status(s, 500, 'Internal Error', e)
+                except Exception as e:
+                    traceback.print_exc()
+                    if not status_sent:
+                        send_it.send_status(s, 500, 'Internal Error', e)
+    
+def mem_list():
+    mem=[]
+    for root, directories, filenames in os.walk(DIRECTORY):
+        for filename in filenames: 
+            mem.append({'root':root,'filename':filename})
+    return mem
+    
+def find_name(mem,name,direct=False):
+    files=[]
+    ids=[]
+    for i in range(len(mem)):
+        if direct:
+            if name == mem[i]['filename']:
+                files.append(mem[i]['filename'])
+                ids.append(i)
+                break
+        else:
+            if name in mem[i]['filename']:
+                files.append(mem[i]['filename'])
+                ids.append(i)
+    return files,ids
+
+def xml_form(files,ids):
+    root = ET.Element('root')
+    
+    if len(files)>0:
+        for file,i in zip(files,ids):
+            ET.SubElement(root, 'result', name=file, id=str(i))
+       
+    return ET.tostring(root)
+    
+def main():
+    server()
+if __name__ == '__main__':
+    main()
+
+
+
+
+# vim: expandtab tabstop=4 shiftwidth=4 
